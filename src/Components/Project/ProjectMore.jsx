ee045b514dd9e77957ea5551452d3ff1b26a85c3
wyntheprogrammer <wyntheprogrammer@gmail.com>
2025-07-26 23:46:52 +0800

    Projects
    Added More projects

--- src/Components/Project/ProjectMore.jsx
+++ src/Components/Project/ProjectMore.jsx
@@ -45,9 +45,6 @@ import mw13 from '../../assets/Project/MakatiLibros/Website/m13.jpeg';
 import mw14 from '../../assets/Project/MakatiLibros/Website/m14.jpeg';
 import mw15 from '../../assets/Project/MakatiLibros/Website/m15.jpeg';
 
-
-
-
 import ma1 from '../../assets/Project/MakatiLibros/Admin/m1.png';
 import ma2_0 from '../../assets/Project/MakatiLibros/Admin/m2.0.png';
 import ma2_1 from '../../assets/Project/MakatiLibros/Admin/m2.1.png';
@@ -145,6 +142,47 @@ import ma17_3 from '../../assets/Project/MakatiLibros/Admin/m17.3.png';
 
 
 
+
+
+import z1 from '../../assets/Project/ZhiList/z1.png';
+import z2 from '../../assets/Project/ZhiList/z2.png';
+import z3_0 from '../../assets/Project/ZhiList/z3.0.png';
+import z3_1 from '../../assets/Project/ZhiList/z3.1.png';
+import z3_2 from '../../assets/Project/ZhiList/z3.2.png';
+import z3_3 from '../../assets/Project/ZhiList/z3.3.png';
+import z3_4 from '../../assets/Project/ZhiList/z3.4.png';
+import z3_5 from '../../assets/Project/ZhiList/z3.5.png';
+import z3_6 from '../../assets/Project/ZhiList/z3.6.png';
+import z3_7 from '../../assets/Project/ZhiList/z3.7.png';
+import z3_8 from '../../assets/Project/ZhiList/z3.8.png';
+import z3_9 from '../../assets/Project/ZhiList/z3.9.png';
+import z3_10 from '../../assets/Project/ZhiList/z3.10.png';
+import z3_11 from '../../assets/Project/ZhiList/z3.11.png';
+import z3_12 from '../../assets/Project/ZhiList/z3.12.png';
+import z3_13 from '../../assets/Project/ZhiList/z3.13.png';
+import z3_14 from '../../assets/Project/ZhiList/z3.14.png';
+import z4_0 from '../../assets/Project/ZhiList/z4.0.png';
+import z4_1 from '../../assets/Project/ZhiList/z4.1.png';
+import z4_2 from '../../assets/Project/ZhiList/z4.2.png';
+import z4_3 from '../../assets/Project/ZhiList/z4.3.png';
+import z5 from '../../assets/Project/ZhiList/z5.png';
+import z6_0 from '../../assets/Project/ZhiList/z6.0.png';
+import z6_1 from '../../assets/Project/ZhiList/z6.1.png';
+import z6_2 from '../../assets/Project/ZhiList/z6.2.png';
+import z6_3 from '../../assets/Project/ZhiList/z6.3.png';
+import z6_4 from '../../assets/Project/ZhiList/z6.4.png';
+import z6_5 from '../../assets/Project/ZhiList/z6.5.png';
+import z6_6 from '../../assets/Project/ZhiList/z6.6.png';
+import z6_7 from '../../assets/Project/ZhiList/z6.7.png';
+import z6_8 from '../../assets/Project/ZhiList/z6.8.png';
+import z7_0 from '../../assets/Project/ZhiList/z7.0.png';
+import z7_1 from '../../assets/Project/ZhiList/z7.1.png';
+import z7_2 from '../../assets/Project/ZhiList/z7.2.png';
+
+
+
+
+
 import nw1 from '../../assets/Project/NuevoLink/Website/n1.jpeg';
 import nw2 from '../../assets/Project/NuevoLink/Website/n2.jpeg';
 import nw3 from '../../assets/Project/NuevoLink/Website/n3.jpeg';
@@ -164,10 +202,6 @@ import nw10_0 from '../../assets/Project/NuevoLink/Website/n10.0.jpeg';
 import nw10_1 from '../../assets/Project/NuevoLink/Website/n10.1.jpeg';
 import nw11_0 from '../../assets/Project/NuevoLink/Website/n11.0.jpeg';
 import nw11_1 from '../../assets/Project/NuevoLink/Website/n11.1.jpeg';
-
-
-
-
 
 import na1 from '../../assets/Project/NuevoLink/Admin/n1.jpeg';
 import na2_0 from '../../assets/Project/NuevoLink/Admin/n2.0.png';
@@ -203,6 +237,21 @@ import na15_1 from '../../assets/Project/NuevoLink/Admin/n15.1.png';
 import na16 from '../../assets/Project/NuevoLink/Admin/n16.png';
 
 
+import f1 from '../../assets/Project/FinanZen/f1.jpg';
+import f2 from '../../assets/Project/FinanZen/f2.jpg';
+import f3 from '../../assets/Project/FinanZen/f3.jpg';
+import f4 from '../../assets/Project/FinanZen/f4.jpg';
+import f5 from '../../assets/Project/FinanZen/f5.jpg';
+import f6 from '../../assets/Project/FinanZen/f6.jpg';
+import f7 from '../../assets/Project/FinanZen/f7.jpg';
+import f8 from '../../assets/Project/FinanZen/f8.jpg';
+import f9_0 from '../../assets/Project/FinanZen/f9.0.jpg';
+import f9_1 from '../../assets/Project/FinanZen/f9.1.jpg';
+import f10 from '../../assets/Project/FinanZen/f10.jpg';
+import f11 from '../../assets/Project/FinanZen/f11.jpg';
+import f12 from '../../assets/Project/FinanZen/f12.jpg';
+import f13 from '../../assets/Project/FinanZen/f13.jpg';
+import f14 from '../../assets/Project/FinanZen/f14.jpg';
 
 
 
@@ -236,20 +285,61 @@ import g11_4 from '../../assets/Project/G/g11.4.png';
 import g11_5 from '../../assets/Project/G/g11.5.png';
 
 
+import a1 from '../../assets/Project/A8/a1.png';
+import a2 from '../../assets/Project/A8/a2.png';
+import a3 from '../../assets/Project/A8/a3.png';
+import a4_0 from '../../assets/Project/A8/a4.0.png';
+import a4_1 from '../../assets/Project/A8/a4.1.png';
+import a4_2 from '../../assets/Project/A8/a4.2.png';
+import a4_3 from '../../assets/Project/A8/a4.3.png';
+import a5_0 from '../../assets/Project/A8/a5.0.png';
+import a5_1 from '../../assets/Project/A8/a5.1.png';
+import a5_2 from '../../assets/Project/A8/a5.2.png';
+import a6_0 from '../../assets/Project/A8/a6.0.png';
+import a6_1 from '../../assets/Project/A8/a6.1.png';
+import a6_2 from '../../assets/Project/A8/a6.2.png';
+import a6_3 from '../../assets/Project/A8/a6.3.png';
+import a7_0 from '../../assets/Project/A8/a7.0.png';
+import a7_1 from '../../assets/Project/A8/a7.1.png';
+import a7_2 from '../../assets/Project/A8/a7.2.png';
+import a8_0 from '../../assets/Project/A8/a8.0.png';
+import a8_1 from '../../assets/Project/A8/a8.1.png';
+import a8_2 from '../../assets/Project/A8/a8.2.png';
+import a8_3 from '../../assets/Project/A8/a8.3.png';
+import a9_0 from '../../assets/Project/A8/a9.0.png';
+import a9_1 from '../../assets/Project/A8/a9.1.png';
+import a10_0 from '../../assets/Project/A8/a10.0.png';
+import a10_1 from '../../assets/Project/A8/a10.1.png';
+import a10_2 from '../../assets/Project/A8/a10.2.png';
+import a10_3 from '../../assets/Project/A8/a10.3.png';
+import a11 from '../../assets/Project/A8/a11.png';
+import a12 from '../../assets/Project/A8/a12.png';
+import a13 from '../../assets/Project/A8/a13.png';
+
+
+
+import ar1 from '../../assets/Project/AliRitzy/ar1.png';
+import ar2 from '../../assets/Project/AliRitzy/ar2.png';
+import ar3 from '../../assets/Project/AliRitzy/ar3.png';
+import ar4 from '../../assets/Project/AliRitzy/ar4.png';
+import ar5 from '../../assets/Project/AliRitzy/ar5.png';
+import ar6 from '../../assets/Project/AliRitzy/ar6.png';
 
 
 import ProjectBox from './ProjectBox'
+import { a } from 'framer-motion/client'
 
 
 const ProjectMore = () => {
 
     return (
-        <div className='snap-start flex flex-col justify-center gap-10 text-white  h-[calc(100vh-5rem)] snap-start'>
+        <div className='flex flex-col items-center justify-center gap-20 text-white  h-auto m-12'>
 
             <span className='text-5xl text-white text-center block hidden-animate-fade'>Projects</span>
 
-            <div className="flex flex-row justify-center gap-10">
+            <div className="w-4/5  flex flex-row flex-wrap justify-center gap-10">
 
+                {/* MakatiLibros Project */}
                 <ProjectBox
                     src={lms}
                     alt="LMS"
@@ -288,6 +378,44 @@ const ProjectMore = () => {
                         ma14_10, ma15_0, ma15_1, ma16_0, ma16_1, ma16_2, ma16_3, ma17_0, ma17_1, ma17_2, ma17_3
                     ]}
                     style={{ "--delay": "300ms" }} />
+
+
+                {/* Zhi Inventory Project */}
+                <ProjectBox
+                    src={lms}
+                    alt=""
+                    title="Zhiyuan Inventory"
+                    description="Zhiyuan Inventory is a web-based inventory system developed during my internship in 2025 at Zhiyuan Enterprise Group Inc., a company that specializes in providing high-quality products to its customers. The system manages a wide variety of products, including different brands of billiard equipment, motorcycle accessories, and anime figurines. The project was built using PHP, MySQL, and JavaScript, and is designed to help the company manage its inventory more efficiently."
+                    features={[
+                        "User Authentication & Role Management: Secure login system for admins and staff with role-based access control.",
+                        "Input Validation: Ensures that user input is accurate and meets defined rules.",
+                        "Shop & Employee Management (CRUD): Create, read, update, and delete shop and employee records.",
+                        "Audit Trails: Logs system activities for accountability and transparency.",
+                        "Search & Filtering: Quickly locate records using dynamic filters and search functions.",
+                        "Data Analytics: Provides visual summaries of monthly sales and performance.",
+                        "Report Generation: Generates summary reports for daily, weekly, monthly, and yearly income, including total counts of sales, stock levels, repaired items, and damaged items.",
+                        "Print Support: Enables printing of clean, organized lists such as shop inventories, product records, and sales reports."
+                    ]}
+
+                    tools={[
+                        "HTML",
+                        "CSS",
+                        "JavaScript",
+                        "PHP",
+                        "MySQL"
+                    ]}
+
+                    images={[
+                        z1, z2, z3_0, z3_1, z3_2, z3_3, z3_4, z3_5,
+                        z3_6, z3_7, z3_8, z3_9, z3_10, z3_11, z3_12,
+                        z3_13, z3_14, z4_0, z4_1, z4_2, z4_3, z5,
+                        z6_0, z6_1, z6_2, z6_3, z6_4, z6_5, z6_6,
+                        z6_7, z6_8, z7_0, z7_1, z7_2
+                    ]}
+                    style={{ "--delay": "300ms" }} />
+
+
+                {/* NuevoLink Project */}
                 <ProjectBox
                     src={bms}
                     alt="BMS"
@@ -322,11 +450,43 @@ const ProjectMore = () => {
                         na14_1, na15_0, na15_1, na16
                     ]}
                     style={{ "--delay": "600ms" }} />
+
+
+                {/* FinanZen Project */}
+                <ProjectBox
+                    src={lms}
+                    alt=""
+                    title="FinanZen"
+                    description="FinanZen is a mobile application calculator with additional features compared to standard calculators. It was developed during my third year, first semester (2023), in the Elective 1 (ELEC 1) subject at the University of Makati (UMak), using Java and Android Studio. The app includes tools for financial planning, unit and currency conversion, and note-taking, making it a versatile companion for everyday use."
+                    features={[
+                        "Basic Calculator: Perform arithmetic operations such as addition, subtraction, multiplication, and division.",
+                        "Unit Conversion: Convert between various measurement units (length, weight, temperature, etc.).",
+                        "Currency Conversion: Convert between different currencies with updatable rates.",
+                        "History: Keeps a record of previous calculations for easy reference.",
+                        "Notes with CRUD: Create, read, update, and delete personal notes.",
+                        "Scan to Text: Convert handwritten or printed text into editable notes using OCR.",
+                        "Voice to Text: Convert spoken words into note text for faster input.",
+                        "Simple Budgeting Tool: Track income and expenses, set budgets, and monitor financial goals."
+                    ]}
+                    tools={[
+                        "XML",
+                        "Java",
+                        "Android Studio",
+                        "SQLite"
+                    ]}
+                    images={[
+                        f1, f2, f3, f4, f5, f6, f7, f8,
+                        f9_0, f9_1, f10, f11, f12, f13, f14
+                    ]}
+                    style={{ "--delay": "300ms" }} />
+
+
+                {/* Gym Management System Project */}
                 <ProjectBox
                     src={gms}
                     alt="GMS"
                     title="GMS"
-                    description="A Gym management system developed using PHP and MySQL to handle memberships, payments, and equipment inventory. It is a fully functional web application created during my second year in 2022 to efficiently manage gym operations."
+                    description="A Gym Management System developed using PHP and MySQL. This project is similar to my previous gym management system built with C#, but this version uses web technologies. It was created during my second year, second semester (2023) at the University of Makati (UMak) for our Application Development (AppDev) subject. The system handles memberships, payments, and equipment inventory. It is a fully functional web application designed to efficiently manage gym operations."
                     features={[
                         "User Authentication & Role Management: Secure login system for admins and staff with role-based access control.",
                         "Input Validation: Ensures user input is correct and meets defined rules for accuracy.",
@@ -350,6 +510,65 @@ const ProjectMore = () => {
                         g11_5
                     ]}
                     style={{ "--delay": "900ms" }} />
+
+
+
+                {/* A8 Management */}
+                <ProjectBox
+                    src={lms}
+                    alt=""
+                    title="A8 Management"
+                    description="A8 Pounder Gym Management System is a desktop application developed using C# during my second year (2022) in the Object-Oriented Programming OOP (OOP) subject at the University of Makati (UMak).
+                                A8 Pounder Gym, located in Pembo, Taguig City, Metro Manila, Philippines, previously relied on manual logbooks to manage their operations, which made tracking and organizing data difficult.
+                                This system provides a fully functional solution for managing gym operations, including member registration, payment tracking, and more. It significantly improves efficiency and accuracy in daily gym activities."
+                    features={[
+                        "User Authentication & Role Management: Secure login system for admins and staff with role-based access control.",
+                        "Input Validation: Ensures user input is correct and meets defined rules for accuracy.",
+                        "Member & Staff Management (CRUD): Manage creation, updates, and deletion of member, coach, and staff records.",
+                        "Search & Filtering: Locate member, coach, staff profiles and payment records quickly.",
+                        "Data Analytics: Visual summaries of monthly income from members and non-members.",
+                        "Report Generation: Generate summary reports for daily, weekly, monthly, and yearly income, as well as total counts of staff, coaches, equipment, members (active and inactive), and non-members.",
+                        "Print Support: Allows printing of organized lists such as members, coaches, staff, equipment, and payment records in a clean, print-ready format.",
+                        "QR Code Generation & Email: Automatically generates a QR code for each registered member and sends it to their email for easy check-in and verification.",
+                        "Email Composer: Allows admins and staff to compose and send emails directly from the system to members for announcements, reminders, and updates.",
+                        "Calendar & Notifications: Integrated calendar for scheduling sessions, payments, and events, with automated notifications for important dates."
+                    ]}
+                    tools={[
+                        "C#", "SQL Server", "Windows Forms"
+                    ]}
+
+                    images={[
+                        a1, a2, a3, a4_0, a4_1, a4_2, a4_3,
+                        a5_0, a5_1, a5_2, a6_0, a6_1, a6_2,
+                        a6_3, a7_0, a7_1, a7_2, a8_0, a8_1,
+                        a8_2, a8_3, a9_0, a9_1, a10_0, a10_1,
+                        a10_2, a10_3, a11, a12, a13
+                    ]}
+                    style={{ "--delay": "300ms" }} />
+
+
+
+                {/* AliRitzy Website */}
+                <ProjectBox
+                    src={lms}
+                    alt=""
+                    title="AliRitzy Website"
+                    description="This was my first project during my first year (2021) in the Web Application Development (WEBAPPS) subject at the University of Makati (UMak). We were instructed to create a website for a selected client.
+                                AliRitzy is an online clothing shop that sells dresses, shorts, skirts, pants, and more through platforms like Facebook and Shopee. The client needed a website to professionally showcase their products, reach a wider audience, and improve customer accessibility beyond social media platforms.
+                                This simple website highlights my skills in HTML and CSS. The project was a great learning experience, allowing me to apply the concepts learned in class to build a functional and visually appealing website."
+                    features={[
+                        "Responsive product gallery for clothing categories"
+                    ]}
+
+                    tools={[
+                        "HTML",
+                        "CSS"
+                    ]}
+
+                    images={[
+                        ar1, ar2, ar3, ar4, ar5, ar6
+                    ]}
+                    style={{ "--delay": "300ms" }} />
 
             </div>
 
